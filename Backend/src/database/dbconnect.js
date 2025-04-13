d37505a28acd12e7f654d92e603b6a02a10c08c7
103122104 <sulabhagarwal1408@gmail.com>
2025-04-13 23:46:25 +0530

    Basic Setup Ready

---
+++ Backend/src/database/dbconnect.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function connectDB(){
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB connected DB host:  ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection Failed : ", error.message);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./database/dbconnect.js";
+import { app } from "./app.js";
+
+connectDB()
+.then(()=> {
+    app.on("error", (err)=>(
+        console.log("Error after connection", err)
+    ))
+
+    app.listen(process.env.PORT || 8000 , ()=>(
+        console.log(`app is listening.`)
+    ) )
+})
+.catch()
